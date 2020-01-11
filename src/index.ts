dbc67917ef4ebb95a13e019db724496919f22883
Grant Orndorff <orndorffgrant@gmail.com>
2020-01-11 15:25:35 -0500

    Initial implementation

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+async function checkRes(res: Response, expectedStatus: number) {
+  if (res.status !== expectedStatus) {
+    throw new Error(`${res.status} ${res.statusText}: ${(await res.text()) || "<no error message>"}`)
+  }
+}
+
+export class JustAuthenticateMe {
+  jamApiUrl: string
+  jsonHeaders: HeadersInit
+
+  constructor(appId: string) {
+    this.jamApiUrl = `https://api.justauthenticate.me/${appId}/`
+    this.jsonHeaders = {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  async initAuth(email: string) {
+    const res = await fetch(`${this.jamApiUrl}authenticate`, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: this.jsonHeaders,
+      body: JSON.stringify({ email })
+    })
+    await checkRes(res, 200)
+  }
+
+  getTokensFromURL(): { idToken: string, refreshToken: string | null } {
+    const params = (new URL(document.location.href)).searchParams;
+    return {
+      idToken: params.get('idToken'),
+      refreshToken: params.get('refreshToken')
+    }
+  }
+
+  async refresh(refreshToken: string): Promise<string> {
+    const res = await fetch(`${this.jamApiUrl}refresh`, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: this.jsonHeaders,
+      body: JSON.stringify({ refreshToken })
+    })
+    await checkRes(res, 200)
+    return (await res.json()).idToken
+  }
+
+  async deleteRefreshToken(userIdToken: string, refreshToken: string) {
+    const res = await fetch(`${this.jamApiUrl}refresh/${refreshToken}`, {
+      method: 'DELETE',
+      cache: 'no-cache',
+      headers: {
+        'Authorization': `Bearer ${userIdToken}`
+      },
+    })
+    await checkRes(res, 204)
+  }
+
+  async deleteAllRefreshTokens(userIdToken: string) {
+    const res = await fetch(`${this.jamApiUrl}refresh`, {
+      method: 'DELETE',
+      cache: 'no-cache',
+      headers: {
+        'Authorization': `Bearer ${userIdToken}`
+      },
+    })
+    await checkRes(res, 204)
+  }
+}
